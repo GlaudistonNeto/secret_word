d904f8029ad92d3bf09af9fee2c6200ecc642f0c
GlaudistonNeto <glaudistonson@gmail.com>
2022-04-27 22:36:44 -0300

    🖥️wip: started_the_game_screen

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,8 +3,31 @@ import './Game.css'
 const Game = ({ verifyLetter }) => {
   return (
     <div className='game'>
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>Finish Game</button>
+      <div className="game">
+        <p className="points">
+          <span>Score: 000</span>
+        </p>
+        <h1>Guess the word: </h1>
+        <h3 className="tip">
+          Tip about the word: <span>Tip...</span>
+        </h3>
+        <div className="wordContainer">
+          <span className="letter">A</span>
+          <span className="blankSquere"></span>
+        </div>
+        <div className="letterContainer">
+          <p>Try to guess a letter of the word: </p>
+          <form>
+            <input type="text" name="letter" maxLength="1" required />
+            <button>Play</button>
+          </form>
+          <div className="wrongLetterContainer">
+            <p>Letters already used: </p>
+            <span>a, </span>
+            <span>b, </span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
